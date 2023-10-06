f768f22c2cec16a5cd48bfc45e0aaf52acac0ad5
Gaganpreet Singh <dmgaganpreet@gmail.com>
2023-10-06 18:26:11 +0530

    C3: Button

---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../Button/Button";
+import styles from "./Navbar.module.css";
+import Logo from "../Logo/Logo";
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <Logo />
+      <Button>Give Feedback</Button>
+    </nav>
+  );
+};
+
+export default Navbar;
